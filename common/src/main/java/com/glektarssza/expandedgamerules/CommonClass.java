0e6f582cbdbe54800e83d8c88f70be6719bb2b58
G'lek Tarssza <glek@glektarssza.com>
2024-09-04 12:32:48 -0600

    Added more doc comments.

--- common/src/main/java/com/glektarssza/expandedgamerules/CommonClass.java
+++ common/src/main/java/com/glektarssza/expandedgamerules/CommonClass.java
@@ -4,7 +4,13 @@ import com.glektarssza.expandedgamerules.platform.Services;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.item.Items;
 
+/**
+ * A common utility class for the mod.
+ */
 public class CommonClass {
+    /**
+     * Common initialization routine for the mod.
+     */
     public static void init() {
         Constants.LOG.info("Hello from Common init on {}! we are currently in a {} environment!",
                 Services.PLATFORM.getPlatformName(), Services.PLATFORM.getEnvironmentName());
