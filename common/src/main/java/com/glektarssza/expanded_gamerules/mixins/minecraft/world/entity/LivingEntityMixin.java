5b6128cb4f097912c67ed257cd90b17570710da5
G'lek Tarssza <glek@glektarssza.com>
2026-04-13 16:53:14 -0600

    First working mixin.

--- common/src/main/java/com/glektarssza/expanded_gamerules/mixins/minecraft/world/entity/LivingEntityMixin.java
+++ common/src/main/java/com/glektarssza/expanded_gamerules/mixins/minecraft/world/entity/LivingEntityMixin.java
@@ -6,21 +6,33 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.waypoints.WaypointTransmitter;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+/**
+ * A mixin into the {@link LivingEntity} class.
+ */
 @Mixin(LivingEntity.class)
-public abstract class LivingEntityMixin extends Entity
-    implements Attackable, WaypointTransmitter {
+public abstract class LivingEntityMixin extends Entity implements Attackable {
+    /**
+     * A constructor to make Java happy.
+     *
+     * @param entityType The type of the entity being constructed.
+     * @param level The world level.
+     */
     protected LivingEntityMixin(EntityType<? extends LivingEntity> entityType,
         Level level) {
         super(entityType, level);
     }
 
+    /**
+     * The injection into the {@code canBeSeenAsEnemy} method.
+     *
+     * @param cir The callback return information.
+     */
     @Inject(method = "canBeSeenAsEnemy", at = @At("RETURN"), cancellable = true)
     public void canBeSeenAsEnemy$adjustReturnValueForPlayers(
         CallbackInfoReturnable<Boolean> cir) {
